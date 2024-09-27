13f86f7ce61ec6c754457647794df9310ee8c3de
Zackary Rippee <zrippee@ut.utm.edu>
2024-09-27 18:55:22 -0500

    Vector Class working most likely need to test

--- HW1/Vector.cpp
+++ HW1/Vector.cpp
@@ -25,9 +25,9 @@ using namespace std;
  * 
  */
 Vector::Vector(){
-  int * vec_ptr = NULL;
-  int vec_size = 0;
-  int vec_capacity = 0;
+  vec_ptr = NULL;
+  vec_size = 0;
+  vec_capacity = 0;
 }
 
 /**
@@ -41,28 +41,62 @@ Vector::Vector(){
  */
 Vector::~Vector(){
   delete[] vec_ptr;
-  int vec_size = 0;
-  int vec_capacity = 0;
+  vec_ptr = NULL;
+  vec_size = 0;
+  vec_capacity = 0;
 }
 
 Vector::Vector(const Vector &other){
-
+  vec_size = other.vec_size;
+  vec_capacity = other.vec_capacity;
+  vec_ptr = new int[vec_capacity];
+  for (int i = 0; i < vec_size; i++){
+    vec_ptr[i] = other.vec_ptr[i];
+  }
 }
 
 Vector& Vector::operator=(const Vector &other){
-
+  if (this != &other){
+    delete[] vec_ptr;
+    vec_ptr = new int[vec_capacity];
+    vec_capacity = other.vec_capacity;
+    for (int i = 0; i < vec_size; i++){
+      vec_ptr[i] = other.vec_ptr[i];
+    }
+  }
+  return * this;
 }
 
 void Vector::push_back(int element){
-
+  reserve(vec_size + 1);
+  vec_ptr[vec_size++] = element; 
 }
 
 void Vector::reserve(int n){
+  if (n > vec_capacity){
+    int new_capacity;
 
+    if (vec_capacity == 0){
+      new_capacity = 1;
+    } else {
+      new_capacity = vec_capacity * 2;
+    }
+
+    int * new_ptr = new int[new_capacity];
+    if (vec_ptr != NULL){
+      for (int i = 0; i < vec_size; i++){
+	new_ptr[i] = vec_ptr[i];
+      }
+    }
+
+    delete[] vec_ptr;
+    vec_ptr = new_ptr;
+    vec_capacity = new_capacity;
+  }
 }
 
 int& Vector::operator[](unsigned int index){
-
+  return vec_ptr[index];
 }
 
 int Vector::size(){
--- HW1/main.cpp
+++ HW1/main.cpp
@@ -5,7 +5,25 @@
 using namespace std;
 
 int main(){
+  Vector Vec;
+  //Tests adding integers into vector, prints them out
+  for (int i = 0; i < 1000; i++){
+    Vec.push_back(i);
+    cout << Vec.capacity() << endl;
+  }
 
+  //Uses Vector Size to print out the Vector
+  for (int i = 0; i < Vec.size(); i++){
+    cout << Vec[i] << endl;
+  }
+  cout << endl;
+
+  //Copying Vector to a Second one
+  Vector VecTwo;
+  VecTwo = Vec;
+
+  
+  
 
   return 0;
 }
