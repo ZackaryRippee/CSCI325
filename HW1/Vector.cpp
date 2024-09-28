9866ab1144942cf1f0e709a85e0b2a97c3791c18
Zackary Rippee <zrippee@ut.utm.edu>
2024-09-27 19:53:48 -0500

    Comments

--- HW1/Vector.cpp
+++ HW1/Vector.cpp
@@ -25,7 +25,7 @@ using namespace std;
  * 
  */
 Vector::Vector(){
-  vec_ptr = NULL;
+  vec_ptr = NULL; //POINTER - points to NULL (safe)
   vec_size = 0;
   vec_capacity = 0;
 }
@@ -40,23 +40,45 @@ Vector::Vector(){
  *
  */
 Vector::~Vector(){
-  delete[] vec_ptr;
-  vec_ptr = NULL;
+  delete[] vec_ptr; //Deallocation of pointer
+  vec_ptr = NULL; //POINTER - points to NULL (safe)
   vec_size = 0;
   vec_capacity = 0;
 }
 
+
+
+/**
+ * Copy Constructor: Initializes a dynamic array of the appropriate
+ *    size/capacity and copies data from other's array to the new array
+ *
+ * @pre existing Vector 
+ * @return  
+ * @post vec_ptr is initialized, and its array contains other.vec_ptr's data,
+ *    vec_size=other.vec_size, and vec_capacity=other.vec_capacity.
+ * 
+ */
 Vector::Vector(const Vector &other){
   vec_size = other.vec_size;
   vec_capacity = other.vec_capacity;
-  vec_ptr = new int[vec_capacity];
+  vec_ptr = new int[vec_capacity]; //POINTER - points to new array with capacity 
   for (int i = 0; i < vec_size; i++){
     vec_ptr[i] = other.vec_ptr[i];
-  }
+   }
 }
 
-Vector& Vector::operator=(const Vector &other){
-  if (this != &other){
+/**
+ * Assignment operator.  Initializes a dynamic array of the appropriate
+ *    size/capacity and copies data from other's array to the new array.
+ *
+ * @pre existing Vector
+ * @return 
+ * @post  vec_ptr is initialized, and its array contains other.vec_ptr's data,
+ *    vec_size=other.vec_size, and vec_capacity=other.vec_capacity.
+ * 
+ */
+Vector& Vector::operator=(const Vector &other){ 
+  if (this != &other){ 
     delete[] vec_ptr;
     vec_ptr = new int[vec_capacity];
     vec_capacity = other.vec_capacity;
@@ -67,11 +89,36 @@ Vector& Vector::operator=(const Vector &other){
   return * this;
 }
 
+
+/**
+ * Similar to std::vector.push_back(int element), stores element at the end of the vector.
+ * If necessary, calls reserve() before adding element to the end of the vector.
+ * @pre existing Vector
+ * @return void
+ * @post Item element has is stored at the end of the vector.
+ * 
+ */
 void Vector::push_back(int element){
   reserve(vec_size + 1);
   vec_ptr[vec_size++] = element; 
 }
 
+/**
+ *  Requests that the vector capacity be resized at least enough to contain n elements.
+ *
+ *    If n is greater than the current capacity, the function causes the container to reallocate
+ *    its storage increasing its capacity to n (or greater).
+ *
+ *    If n is less than or equal to the current capacity, the call does nothing (no reallocation
+ *    or change in vector capacity).
+ *
+ *    This function has no effect on vector size and cannot alter its elements.
+ *
+ * @pre Existing Vector 
+ * @return void
+ * @post Vector capacity is now n.  Vector size (and contents) remains the same.
+ * 
+ */
 void Vector::reserve(int n){
   if (n > vec_capacity){
     int new_capacity;
@@ -95,14 +142,39 @@ void Vector::reserve(int n){
   }
 }
 
+
+/**
+ * Returns a reference to the element at position indexReturns a reference to the element at position index
+ *
+ * @pre
+ * @return element reference at position index
+ * @post  
+ * 
+ */
 int& Vector::operator[](unsigned int index){
   return vec_ptr[index];
 }
 
+/**
+ * Returns the current number of elements in the vector
+ *
+ * @pre
+ * @return int
+ * @post  
+ * 
+ */
 int Vector::size(){
   return vec_size;
 }
 
+/**
+ * Returns the currently allocated storage capacity of the vector.
+ *
+ * @pre
+ * @return int
+ * @post  
+ * 
+ */
 int Vector::capacity(){
   return vec_capacity;
 }
--- HW1/main.cpp
+++ HW1/main.cpp
@@ -1,29 +1,57 @@
-
+/**
+ *
+ *@file main.cpp
+ *@author Zack Rippee
+ *@date 2024-09-27
+ *@brief Testing for Vector class
+ */
 #include <iostream>
 #include <fstream>
 #include "Vector.h"
 using namespace std;
 
+
+
+/**
+ * main: Runs tests for operator[], size() in a loop , and assignment and copy constructor both showing Deep Copy
+ *
+ * @pre Vector class
+ * @return int return 0 (end program)
+ * @post 
+ * 
+ */
 int main(){
   Vector Vec;
-  //Tests adding integers into vector, prints them out
+  //Tests adding integers into vector, prints out the capacity to test it's doubling
   for (int i = 0; i < 1000; i++){
     Vec.push_back(i);
-    cout << Vec.capacity() << endl;
+    cout << "New insert: " << Vec[i] << " Capacity: " << Vec.capacity() << endl;
   }
+  cout << endl;
 
   //Uses Vector Size to print out the Vector
+  cout << "Vector One (Vec): " << endl;
   for (int i = 0; i < Vec.size(); i++){
     cout << Vec[i] << endl;
   }
   cout << endl;
 
-  //Copying Vector to a Second one
-  Vector VecTwo;
-  VecTwo = Vec;
+  //Assignment Test (Deep Copy)
+  Vector VecTwo = Vec;
+  //Prints out VecTwo to make sure it assigned properly
+  cout << "Vector Two (VecTwo): " << endl;
+  for (int i = 0; i < VecTwo.size(); i++){
+    cout << VecTwo[i] << endl;
+  }
+  cout << endl;
 
-  
-  
-
+  //Using copy constructor
+  Vector VecCopy(Vec);  
+  cout << "Vector One copy (VecCopy): " << endl;
+  //Prints out VecCopy to make sure it copied
+  for (int i = 0; i <VecCopy.size(); i++){
+    cout << VecCopy[i] << endl;
+  }
+  cout << endl;
   return 0;
 }
